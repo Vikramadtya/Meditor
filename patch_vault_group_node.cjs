708eec453626cae0870c0cb47c902e0ab5ae8f15
Vikramaditya Singh <86847191+Vikramadtya@users.noreply.github.com>
2026-09-02 11:51:47 +0530

    chore: cleanup patch scripts

--- patch_vault_group_node.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let file = fs.readFileSync('src/components/vault/sidebar/VaultGroupNode.jsx', 'utf8');
-
-file = file.replace(
-  'openCreateVaultItemModal("container", group.path);',
-  'openCreateVaultItemModal("auto", group.path);'
-);
-
-fs.writeFileSync('src/components/vault/sidebar/VaultGroupNode.jsx', file);
--- patch_vault_node.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let file = fs.readFileSync('src/components/vault/sidebar/VaultNode.jsx', 'utf8');
-
-file = file.replace(
-  'openCreateVaultItemModal("container", item.path);',
-  'openCreateVaultItemModal("auto", item.path);'
-);
-
-fs.writeFileSync('src/components/vault/sidebar/VaultNode.jsx', file);
