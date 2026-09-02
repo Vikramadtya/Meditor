2b364ae7ed91d2ec7c1390597b688981ca6d7343
Vikramaditya Singh <86847191+Vikramadtya@users.noreply.github.com>
2026-09-02 10:52:36 +0530

    chore: remove patch scripts

--- patch_open_file.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let file = fs.readFileSync('src/store/actions/editorActions.js', 'utf8');
-file = file.replace(
-  '      if (vaultItem) existing.vaultItem = vaultItem;',
-  '      if (vaultItem) { useStore.setState((s) => { const t = s.tabs.find(x => x.id === existing.id); if (t) t.vaultItem = vaultItem; }); }'
-);
-fs.writeFileSync('src/store/actions/editorActions.js', file);
--- patch_regex.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let file = fs.readFileSync('src/store/actions/editorActions.js', 'utf8');
-
-file = file.replace(/const match = text\.match\(\/\^---\n\[\\s\\S\]\*\?\\n---\\n\/\);/, 'const match = text.match(/^---\\n[\\\\s\\\\S]*?\\n---\\n/);');
-
-fs.writeFileSync('src/store/actions/editorActions.js', file);
