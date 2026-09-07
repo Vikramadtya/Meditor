fb1de6c9d8afe0ec4ad04a81d0020043c702d7f2
Vikramaditya Singh <86847191+Vikramadtya@users.noreply.github.com>
2026-09-07 18:51:48 +0530

    fix: unhide generic directories like notes, assets, dist from folder sidebar

--- patch-mutation.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/domains/vault/application/VaultMutationUseCase.js', 'utf8');
-code = code.replace(/window\.Neutralino\.filesystem\.removeDirectory/g, 'fileSystem.removeDirectory');
-fs.writeFileSync('src/domains/vault/application/VaultMutationUseCase.js', code);
